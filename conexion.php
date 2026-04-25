3e97eba6af33f56778b98443a086adfc99fa9b4f
Alan21-crypto <4l4n0502@gmail.com>
2026-04-25 00:30:59 -0600

    conexion.php

--- conexion.php
+++ conexion.php
@@ -1,18 +1,24 @@
 <?php
-include ("conexion1.php");
-	$_host = "localhost";
-	$_user = "root";
-	$_pass = "";
-	$_BD = "pasteleria";
+    include("conexion.php");
+    $_nombre = "";
+    $_producto = "";
+    $_cantidad = "";
+    $_celular = "";
+    $_comentarios = "";
 
-//Crear la conexion
-	$_con = mysqli_connect($_host, $_user, $_pass, $_BD);
+    if(isset($_POST['Enviar'])) {
+        $_nombre = ($_POST['nombre']);
+        $_producto = ($_POST['producto']);
+        $_cantidad = ($_POST['cantidad']);
+        $_celular = ($_POST['celular']);
+        $_comentarios = ($_POST['comentarios']);
+    }
+    $_sql = "INSERT INTO formulario(nombre, producto, cantidad, celular, comentarios) VALUES ('$_nombre', '$_producto', '$_cantidad', '$_celular', '$_comentarios')";
+    $_query = mysqli_query($_con,$_sql);
 
-	if(!$_con){
-		die("Error de Conexion".mysqli_connect_error());
-	}else{
-		echo "Conexion perdida";
-	}
-
-	mysqli_set_charset($_con, "utf8");
+    if($_query){
+        echo " Usuario Insertado Correctamente ";
+    } else {
+        echo " Usuario no Agregado";
+    }
 ?>
