3970896f927af9d08b147061c29e5d19f9c9d0d4
Alan21-crypto <4l4n0502@gmail.com>
2026-04-25 01:12:09 -0600

    conexion.php

--- conexion.php
+++ conexion.php
@@ -15,13 +15,14 @@
         $_cantidad = ($_POST['cantidad']);
         $_celular = ($_POST['celular']);
         $_comentarios = ($_POST['comentarios']);
-    }
+    
     $_sql = "INSERT INTO formulario(nombre, producto, cantidad, celular, comentarios) VALUES ('$_nombre', '$_producto', '$_cantidad', '$_celular', '$_comentarios')";
     $_query = mysqli_query($_con,$_sql);
-
+    
     if($_query){
         echo " Usuario Insertado Correctamente ";
     } else {
         echo " Usuario no Agregado";
     }
+}
 ?>
