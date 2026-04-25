c61bf154e9c52d9d05356470b1e774f4c45a4957
Alan21-crypto <4l4n0502@gmail.com>
2026-04-25 01:36:08 -0600

    conexion1.php

--- conexion1.php
+++ conexion1.php
@@ -1,10 +1,14 @@
 <?php
-// Usamos getenv() que es el método más seguro en Railway
 $host = getenv('MYSQLHOST');
-$port = getenv('MYSQLPORT');
 $user = getenv('MYSQLUSER');
 $pass = getenv('MYSQLPASSWORD');
-$db = getenv('MYSQL_DATABASE');
+$db   = getenv('MYSQL_DATABASE');
+$port = getenv('MYSQLPORT');
+
+// Esto nos dirá si Railway te está dando los datos o no
+if (!$user) {
+    die("Error: Railway no está enviando las variables de entorno al código.");
+}
 
 $con = mysqli_connect($host, $user, $pass, $db, $port);
 
