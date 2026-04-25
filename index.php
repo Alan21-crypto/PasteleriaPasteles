781f3d729195b31501689265197a0ff791bbd1e4
Alan21-crypto <4l4n0502@gmail.com>
2026-04-25 00:15:34 -0600

    index.php

--- index.php
+++ index.php
@@ -1,28 +1,4 @@
 <!DOCTYPE html>
-<?php
-    include("conexion.php");
-    $_nombre = "";
-    $_producto = "";
-    $_cantidad = "";
-    $_celular = "";
-    $_comentarios = "";
-
-    if(isset($_POST['Enviar'])) {
-        $_nombre = ($_POST['nombre']);
-        $_producto = ($_POST['producto']);
-        $_cantidad = ($_POST['cantidad']);
-        $_celular = ($_POST['celular']);
-        $_comentarios = ($_POST['comentarios']);
-    }
-    $_sql = "INSERT INTO formulario(nombre, producto, cantidad, celular, comentarios) VALUES ('$_nombre', '$_producto', '$_cantidad', '$_celular', '$_comentarios')";
-    $_query = mysqli_query($_con,$_sql);
-
-    if($_query){
-        echo " Usuario Insertado Correctamente ";
-    } else {
-        echo " Usuario no Agregado";
-    }
-?>
 
 <html lang="es"> 
 <head> 
