262179576a50c716f6a21fcecfc89ea06ebe0b2b
Alan21-crypto <4l4n0502@gmail.com>
2026-04-25 01:31:01 -0600

    conexion1.php

--- conexion1.php
+++ conexion1.php
@@ -4,7 +4,7 @@ $host = getenv('MYSQLHOST');
 $port = getenv('MYSQLPORT');
 $user = getenv('MYSQLUSER');
 $pass = getenv('MYSQLPASSWORD');
-$db  = getenv('MYSQL_DATABASE');
+$db = getenv('MYSQL_DATABASE');
 
 $con = mysqli_connect($host, $user, $pass, $db, $port);
 
